9711246478b30f102e0876609a4608b5ce2c0773
LFY <751502495@qq.com>
2020-05-30 14:35:40 +0800

    add gitignore

---
+++ src/ast/expression/rvalue/literal/charLiteral.go
@@ -0,0 +1,34 @@
+package literal
+
+import (
+	"Compiler/src/ast/expression/rvalue"
+	"Compiler/src/global"
+	"fmt"
+)
+
+type CharLiteral struct {
+	rvalue.RValue
+	Type 	global.SymbolType
+	Value 	uint8
+}
+
+
+func (c CharLiteral) GeneRVCode() {
+
+}
+
+func (c CharLiteral) RvalueIR() string {
+	result := fmt.Sprintf("%d", c.Value)
+	return result
+}
+
+func (c CharLiteral) GetType() global.SymbolType {
+	return c.Type
+}
+
+func CreateStrLiteral(value uint8) CharLiteral {
+	var charLiteral CharLiteral
+	charLiteral.Value = value
+	charLiteral.Type = global.CHAR_LITERAL
+	return charLiteral
+}
